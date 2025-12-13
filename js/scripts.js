2f61799e9fa001e99666f2cea6d937403ad6ab37
Mayzer <Tolik.Korzunov@yandex.ru>
2025-12-13 15:12:01 +0300

    Added why-dlex-cards swiper

--- js/scripts.js
+++ js/scripts.js
@@ -60,3 +60,89 @@ if (video) {
     video.addEventListener('click', toggleVideo);
 }
 
+
+
+
+
+// Swiper
+
+const whyDlexCards = document.querySelector('.why-dlex-cards');
+
+if (whyDlexCards) {
+    const paginationContainer = document.querySelector('.pagination-dots');
+    const prevBtn = document.querySelector('.pagination-arrow--prev');
+    const nextBtn = document.querySelector('.pagination-arrow--next');
+
+    function renderPagination(swiper) {
+        paginationContainer.innerHTML = '';
+
+        swiper.snapGrid.forEach((_, index) => {
+            const dot = document.createElement('button');
+            dot.className = 'pagination-dot';
+
+            if (index === swiper.snapIndex) {
+            dot.classList.add('pagination-dot--active');
+            }
+
+            dot.addEventListener('click', () => {
+            swiper.slideTo(index);
+            });
+
+            paginationContainer.appendChild(dot);
+        });
+    }
+
+    function updatePagination(swiper) {
+        const dots = paginationContainer.querySelectorAll('.pagination-dot');
+
+        dots.forEach((dot, index) => {
+            dot.classList.toggle(
+            'pagination-dot--active',
+            index === swiper.snapIndex
+            );
+        });
+    }
+
+    function updateArrows(swiper) {
+        prevBtn.classList.toggle('disabled', swiper.isBeginning);
+        nextBtn.classList.toggle('disabled', swiper.isEnd);
+    }
+
+    const whyDlexSwiper = new Swiper(whyDlexCards, {
+        slidesPerView: 4,
+        spaceBetween: 24,
+        speed: 600,
+
+        navigation: {
+            nextEl: '.pagination-arrow--next',
+            prevEl: '.pagination-arrow--prev',
+        },
+
+        on: {
+            init(swiper) {
+            renderPagination(swiper);
+            updatePagination(swiper);
+            updateArrows(swiper);
+            },
+
+            slideChange(swiper) {
+            updatePagination(swiper);
+            updateArrows(swiper);
+            },
+
+            resize(swiper) {
+            renderPagination(swiper);
+            updatePagination(swiper);
+            updateArrows(swiper);
+            }
+        },
+
+        breakpoints: {
+            0: { slidesPerView: 1.2 },
+            576: { slidesPerView: 2 },
+            992: { slidesPerView: 3 },
+            1200: { slidesPerView: 4 },
+        }
+    });
+}
+
