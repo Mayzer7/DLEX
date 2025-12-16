93d4c77d76f2d21b05eea64d403bc46e5fd3c686
Mayzer <Tolik.Korzunov@yandex.ru>
2025-12-16 12:35:05 +0300

    Finished adaptive

--- js/scripts.js
+++ js/scripts.js
@@ -238,8 +238,9 @@ if (whyDlexCards) {
 
         breakpoints: {
             0: { slidesPerView: 1.2 },
-            576: { slidesPerView: 2 },
-            992: { slidesPerView: 3 },
+            500: { slidesPerView: 1.6 },
+            601: { slidesPerView: 2 },
+            991: { slidesPerView: 3 },
             1200: { slidesPerView: 4 },
         }
     });
